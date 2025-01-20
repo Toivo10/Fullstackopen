6e33c910a4904b451b600e20ef5e69bc177dbba0
Tom Pippuri <tapipp@utu.fi>
2025-01-20 18:36:50 +0200

    Tehtävä 1.2 tehty

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>{props.course}</p>
+    </div>
+  )
+}
+const Part = ({part,exercises}) => {
+  return (
+    <div>
+      <p>{part} {exercises}</p>
+    </div>
+  )
+}
+
+const Content = () =>{
+  return (
+    <div>
+      <Part part="Fundamentals of React" exercises={10}/>
+      <Part part="Using props to pass data" exercises={7}/>
+      <Part part="State of a component" exercises={14}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  
+  return (
+    <div>
+      <Header course="Half Stack application development" />
+      <Content/>
+      <Total exercises1={10} exercises2={7} exercises3={14} />
+    </div>
+  )
+}
+
+export default App
